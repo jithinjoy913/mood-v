2e9191a9c0ee295f8bdef40bbd19943236f67cdc
Jithin Joy <jithinjoy913@gmail.com>
2025-04-14 20:48:18 +0530

    Add project files

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   optimizeDeps: {
+//     exclude: ['lucide-react'],
+//   },
+//   base: '/mood-verse/',
+// });
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
+  base: '/', // <- Change this line
+});
